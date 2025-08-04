9acb6a300890aefb747ca32840ec70667f13a047
kyle-dev-47 <amentotech043@gmail.com>
2025-08-04 16:42:02 +0500

    Add GitHub Actions workflow for Cloudflare deployment and update package scripts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-import { Hono } from 'hono'
-import { bearerAuth } from 'hono/bearer-auth'
-import crisisRoutes from './routes/crisis_helplines.routes'
+import { Hono } from 'hono';
+import { bearerAuth } from 'hono/bearer-auth';
+import crisisRoutes from './routes/crisis_helplines.routes';
 import { cors } from 'hono/cors';
 
 type Bindings = {
-  BEARER_TOKEN: string
-  COUNTRY_CSV_URL: string
-  HELPLINE_CSV_URL: string
-}
+  BEARER_TOKEN: string;
+  COUNTRY_CSV_URL: string;
+  HELPLINE_CSV_URL: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-// cors
-app.use("*", cors());
+app.use('*', cors());
 
-// routes
 app.use('/*', async (c, next) => {
-  const token = c.env.BEARER_TOKEN
-  return bearerAuth({ token })(c, next)
-})
+  const token = c.env.BEARER_TOKEN;
+  return bearerAuth({ token })(c, next);
+});
 
-app.route('/crisis_helplines', crisisRoutes)
+app.use('*', async (c, next) => {
+  c.set("COUNTRY_CSV_URL", c.env.COUNTRY_CSV_URL);
+  c.set("HELPLINE_CSV_URL", c.env.HELPLINE_CSV_URL);
+  await next();
+});
 
-export default app
+app.route('/crisis_helplines', crisisRoutes);
+
+export default app;
